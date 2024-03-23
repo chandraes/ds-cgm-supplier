42a87f73518c3e842d0f1dbd37f9aec241db5efb
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-03-23 21:21:38 +0700

    Remove commented code for checking PPn Masukan in cutoff function

--- app/Http/Controllers/NotaTagihanController.php
+++ app/Http/Controllers/NotaTagihanController.php
@@ -97,10 +97,6 @@ class NotaTagihanController extends Controller
             'estimasi_pembayaran' => 'required',
         ]);
 
-        // if ($invoice->ppn_masukan > 0) {
-        //     return redirect()->back()->with('error', 'Tagihan ini memiliki PPn Masukan, silahkan lakukan transaksi PPn Masukan terlebih dahulu!');
-        // }
-
         $store = InvoiceTagihan::cutoff($invoice, $data);
 
         return redirect()->back()->with($store['status'], $store['message']);
